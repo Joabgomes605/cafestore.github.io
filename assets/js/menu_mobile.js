bdc8e4bd4ffe22d0793807e56a56189a20b271a5
joabegomes605 <joabegomes605@gmail.com>
2022-11-03 22:10:15 -0300

    subindo a página cafestore

---
+++ assets/js/menu_mobile.js
@@ -0,0 +1,8 @@
+const btnMobile = document.getElementById('btn_mobile');
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
